99612840da92a4ad7c3dddc1aebd5490af93523e
e7v1s1 <elvismucaj32@gmail.com>
2024-12-19 07:10:41 -0800

    lesson26.1

--- lessons/javascript/main.js
+++ lessons/javascript/main.js
@@ -1,11 +1,46 @@
 console("Hello World")
 
-var a=10;
-var b=20;
-var D= a+b
+// var a=10;
+// var b=20;
+// var D= a+b
 
-console.log(D)
+// console.log(D)
 
 
-var a= 50;
-console.log(a)
+// var a= 50;
+// console.log(a)
+
+
+// var x =20;
+// x +=30;
+// console.log(x)
+
+// var y=100;
+// var c=50
+// var n= y-c;
+// console.log(n)
+
+// var a=40;
+// a **= 2;
+// console.log(a);
+
+var x=100;
+var y=1000;
+ console.log(z<=x);
+
+ var firstname= "Arbnorja";
+ var lastname= "Gruja e Elvisit";
+ console.log(firstname + "" + lastname);
+  
+var age = 20;
+console.log(age=20 && <=65);
+
+var var 1= 20;
+var var 2 = 30;
+console.log(var1 = 20; || var2 = 30 );
+
+
+
+ 
+
+
