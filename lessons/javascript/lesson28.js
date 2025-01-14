38dc0ff8cd43efa317a41ed43825efab1908632e
e7v1s1 <elvismucaj32@gmail.com>
2025-01-14 16:19:11 +0100

    lesson28.1

--- lessons/javascript/lesson28.js
+++ lessons/javascript/lesson28.js
@@ -1,30 +1,58 @@
-/*var button1 = document.getElementById("btn")
+// /*var button1 = document.getElementById("btn")
 
-button1.onclick = function(){
-    alert(HELLO);
+// button1.onclick = function(){
+//     alert(HELLO);
+// }
+
+// button1.addEventListener('click', function(){
+//     alert('hello world')
+// }*/
+
+//  var button2 = document.getElementById("btn1");
+//  var button3 = document.getElementById("btn2");
+//  var button4 = document.getElementById("btn3");
+//  var v_button = document.getElementById('button1');
+ 
+
+//  button2.onmouseover = function(){
+//     alert ('over');
+//  }
+//  button3.onmouseleave = function(){
+//     alert ('leave');
+//  }
+//  button4.onmousewheel = function(){
+//     alert ('wheel');
+//  }
+//  var v_button=document.getElementById('button1');
+//  var v_button=document.getElementById('txt1');
+
+//   v_button.onclick= function(){
+// //    v_text.style.color="red";
+// //    v_text.style.backgroundColor="lightgrey";
+// //    v_text.style.fontSize="100px";
+// //    v_text.style.padding="200px";   
+// v_text.setAttribute("class")
+// }
+//  v_text.onmouseover= (function) {
+//    v_text.style.cssText="color:red; background-color:blue; text-align: center; font-size: 100px; padding: 20px";
+//  }
+var btn_circle=document.getElementById('btnCricle')
+var btn_rect=document.getElementById('btnRect')
+var btn_triangle=document.getElementById('btnTriangle')
+
+var circle=document.getElementById(circle)
+var rect=document.getElementById(Rectangle)
+var triangle=document.getElementById(Triangle)
+
+
+ btn_circle.onclick=function(){
+   circle.setAttribute("class " , "shape_circle")
+}
+btn_rect.onclick=function(){
+   Rectangle.setAttribute("class " , "shape_rect")
+}
+btn_triangle.onclick=function(){
+   circle.setAttribute("class " , "shape_triangle")
 }
 
-button1.addEventListener('click', function(){
-    alert('hello world')
-}*/
-
- var button2 = document.getElementById("btn1");
- var button3 = document.getElementById("btn2");
- var button4 = document.getElementById("btn3");
- var v_button = document.getElementById('button1');
- 
-
- button2.onmouseover = function(){
-    alert ('over');
- }
- button3.onmouseleave = function(){
-    alert ('leave');
- }
- button4.onmousewheel = function(){
-    alert ('wheel');
- }
-
- 
-
-
-
+circle.onclick
